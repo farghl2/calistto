48bf8b27a58827099cbd16cafeaeb6929a129562
islamrockaidev <islamrockaidev@gmail.com>
2026-01-19 23:23:00 +0200

    fix projec single page

--- src/app/[locale]/projects/[id]/page.tsx
+++ src/app/[locale]/projects/[id]/page.tsx
@@ -2,11 +2,6 @@ import { notFound } from 'next/navigation';
 import { projectsData } from '@/src/shared/constans/data';
 import SingleProject from '@/src/features/projects/components/SingleProject';
 
-export async function generateStaticParams() {
-  return projectsData.map((project) => ({
-    id: project.id.toString(),
-  }));
-}
 
 export default async function ProjectPage({ params }: { params: Promise<{ id: string }> }) {
   const { id } = await params;
